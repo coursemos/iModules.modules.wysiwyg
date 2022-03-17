c98ae0981e841eea2d1275757bca763038cad983
arzzcom <arzz@arzz.com>
2022-03-17 17:09:27 +0900

    모듈 클래스 기본컨셉

---
+++ scripts/ModuleWysiwyg.js
@@ -0,0 +1,13 @@
+/**
+ * 이 파일은 아이모듈 게시판모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 위지윅에디터모듈 자바스크립트 클래스를 정의한다.
+ *
+ * @file /modules/wysiwyg/scripts/ModuleWysiwyg.php
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2022. 2. 9.
+ */
+class ModuleWysiwyg extends Module {
+	
+}
