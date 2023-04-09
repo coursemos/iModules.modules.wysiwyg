75f1ed34d27bfd83adfaf1bc56a424ba827ae566
arzzcom <arzz@arzz.com>
2023-04-10 08:47:33 +0900

    위지윅에디터모듈 클래스 추가

--- scripts/Wysiwyg.ts
+++ scripts/Wysiwyg.ts
@@ -1,20 +1,23 @@
 /**
- * 이 파일은 아이모듈 게시판모듈의 일부입니다. (https://www.imodules.io)
+ * 이 파일은 아이모듈 위지윅에디터모듈의 일부입니다. (https://www.imodules.io)
  *
- * 위지윅에디터모듈 자바스크립트 클래스를 정의한다.
+ * 위지윅에디터모듈 클래스를 정의한다.
  *
  * @file /modules/wysiwyg/scripts/Wysiwyg.ts
  * @author Arzz <arzz@arzz.com>
  * @license MIT License
- * @modified 2022. 2. 9.
+ * @modified 2023. 4. 10.
  */
-Modules.set(
-    class wysiwyg extends Module {
-        init() {
-            console.log('Editor INIT!', this.dom);
-            if (this.dom !== null) {
-                console.log(this.dom);
+namespace modules {
+    export namespace wysiwyg {
+        export class Wysiwyg extends Module {
+            init() {
+                // @todo 에디터 초기화
+                console.log('Editor INIT!', this.$dom);
+                if (this.$dom !== null) {
+                    console.log(this.$dom);
+                }
             }
         }
     }
-);
+}
